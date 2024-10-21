c67715b13ada6fb36244233698c2513e7fa85e7e
Andrii Koliush <koliush2006@gmail.com>
2024-10-21 14:38:54 +0200

    updates

---
+++ 03-TextDocumentFormats/main.py
@@ -0,0 +1,16 @@
+# from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+df = pd.read_csv('C:\\Users\\s-A013-24.CAMPUS.001\\Desktop\\data-notations\\03-TextDocumentFormats\\coursesA.csv')
+df1 = pd.read_csv('C:\\Users\\s-A013-24.CAMPUS.001\\Desktop\\data-notations\\03-TextDocumentFormats\\coursesA.csv')
+
+df2 = pd.read_csv('C:\\Users\\s-A013-24.CAMPUS.001\\Desktop\\data-notations\\03-TextDocumentFormats\\students.csv')
+
+# final = pd.concat([df, df1], axis=1)
+
+# final.to_csv('coursesAB.csv', index=False)
+
+print(df2)
+
+
+
